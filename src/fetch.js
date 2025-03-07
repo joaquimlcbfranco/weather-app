8417d0e855431e77bcb90a5dcc557ffc8dc3b364
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-03-07 16:08:14 +0000

    Wrap functions inside IIFE

--- src/fetch.js
+++ src/fetch.js
@@ -1,33 +1,46 @@
-async function getData(location) {
-  try {
-    const data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=UQEYSE2MNGKTQ34PRDEX24VUD`);
-    if (!data.ok) {
-      throw new Error('Error obtaining data');
+const fetchData = (() => {
+  async function getData(location) {
+    try {
+      const data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next7days?key=UQEYSE2MNGKTQ34PRDEX24VUD`);
+      if (!data.ok) {
+        throw new Error('Error obtaining data');
+      }
+      else {
+        const results = await data.json();
+        console.log(results)
+        return results
+      }
     }
-    else {
-      const results = await data.json();
-      return results
+    catch (error) {
+      console.log(error)
     }
   }
-  catch (error) {
-    console.log(error)
+    
+  function processDay(data) {
+    const location = data.resolvedAddress
+    const description = data.currentConditions.conditions
+    const temp = data.currentConditions.temp
+    const precip= data.currentConditions.precipprob
+    const windspeed = data.currentConditions.windspeed
+    
+    return {
+      location,
+      description,
+      temp,
+      precip,
+      windspeed,
+    }
   }
-}
-  
-function processDay(data) {
-  const location = data.resolvedAddress
-  const description = data.currentConditions.conditions
-  const temp = data.currentConditions.temp
-  const precip= data.currentConditions.precipprob
-  const windspeed = data.currentConditions.windspeed
-  
-  return {
-    location,
-    description,
-    temp,
-    precip,
-    windspeed,
-  }
-}
 
-export { getData, processDay }
+  function processWeek(data) {
+    console.log(data.days);
+  }
+
+  return {
+    getData,
+    processDay,
+    processWeek,
+  }
+})();
+
+export default fetchData
