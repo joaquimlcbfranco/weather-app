a58f7882ba69d8babc16cc73a139b2dd5332a9e3
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-03-07 15:20:24 +0000

    Import functions from dom.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles.css"
 import { getData, processDay } from "./fetch.js"
+import dom from './dom.js'
 
 window.getData = getData;
 window.processDay = processDay;
+window.dom = dom;
 
