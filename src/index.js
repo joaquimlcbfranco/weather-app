143529ef7b2428b8808e6e1c23bd0b915c6eaf1a
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-03-10 11:47:39 +0000

    Fetch data when app is first started

--- src/index.js
+++ src/index.js
@@ -4,3 +4,10 @@ import dom from "./dom.js";
 
 window.fetchData = fetchData;
 window.dom = dom;
+
+fetchData.getData('Europe')
+.then((result) => {
+  dom.displayStats(result)
+  dom.displayWeek(result)
+})
+
