88d71595b45c57b5933875454ed598ded980002f
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-03-10 11:26:37 +0000

    Use require.context and run build to process icons

--- webpack.common.js
+++ webpack.common.js
@@ -2,31 +2,31 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
+  entry: "./src/index.js",
+  output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-    },
-    plugins: [
+  },
+  plugins: [
     new HtmlWebpackPlugin({
-        template: "./src/template.html",
+      template: "./src/template.html",
     }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
-    module: {
-        rules: [
-            {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-            },
-            {
-            test: /\.html$/i,
-            loader: "html-loader",
-            },
-            {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: "asset/resource",
-            },
-        ],
-    },
-}
+  },
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,11 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: "development",
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
-        static: './dist',
-    },
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    static: "./dist",
+  },
 });
