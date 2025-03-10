218b1d2236aee32983853687aa2341b51ca65e9a
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-03-10 11:27:40 +0000

    Test require.context

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import "./styles.css"
-import fetchData from "./fetch.js"
-import dom from './dom.js'
+import "./styles.css";
+import fetchData from "./fetch.js";
+import dom from "./dom.js";
 
 window.fetchData = fetchData;
 window.dom = dom;
-
